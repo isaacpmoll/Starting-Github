d87cb840baf4924bde43111a4582a72023d2715d
isaacpmoll <73903177+isaacpmoll@users.noreply.github.com>
2021-01-20 12:31:16 -0500

    Update DoThis.py

--- DoThis.py
+++ DoThis.py
@@ -3,3 +3,17 @@
 # pi to the power of the first number 
 # or the square root of the second number.
 # then commit and push it back up to github.
+import math
+
+print("Isaac Moll")
+
+num1 = float(input("Gimme number one: "))
+num2 = float(input("Gimme number two: "))
+
+num1 = (math.pi)**num1
+num2 = math.sqrt(num2)
+
+if num1 > num2:
+	print(num1)
+else:
+	print(num2)
